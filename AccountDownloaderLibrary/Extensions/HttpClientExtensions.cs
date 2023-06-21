271211aefd9cffc34aa353e3a4a94bfd5f05eb91
GuVAnj8Gv3RJ <private@private.com>
2023-06-21 03:03:25 -0700

    feat(mime): move files to the correct extension after downloading

--- AccountDownloaderLibrary/Extensions/HttpClientExtensions.cs
+++ AccountDownloaderLibrary/Extensions/HttpClientExtensions.cs
@@ -5,14 +5,30 @@ namespace AccountDownloaderLibrary
     // From: https://stackoverflow.com/a/66270371/2095344
     public static class HttpClientExtensions
     {
+        // TODO: now that this does some custom logic (figures out the extension of the file), this should NOT be an extension method.
+        // It is specialized for files from Neos.
+        // Move it to its own class.
         public static async Task DownloadFileTaskAsync(this HttpClient client, Uri uri, string FileName)
         {
-            using var s = await client.GetStreamAsync(uri);
-            var extension = MimeDetector.Instance.MostLikelyFileExtension(s);
+            using (var s = await client.GetStreamAsync(uri))
+            {   
+                using (var fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    await s.CopyToAsync(fs);
+                }
+            }
 
-            using var fs = new FileStream(FileName + "." + extension, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            // TODO: I hate that we move after downloading.
+            // MimeDetective does have a stream reading overload. When It is used, it seems to "corrupt" or halt the stream
+            // The resultant file then has 0 bytes.
+            // I don't understand streams enough to figure this out, but I would love to save the file operations here.
+            //
+            // At least the file move should be async before this is released.
+            // #HelpWanted
 
-            await s.CopyToAsync(fs);
+            var extension = MimeDetector.Instance.MostLikelyFileExtension(FileName);
+            File.Move(FileName, FileName + "." + extension);
+
         }
     }
 }
