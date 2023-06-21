197e7294a913752025311257dba73fb964dac7ec
GuVAnj8Gv3RJ <private@private.com>
2023-06-21 01:18:03 -0700

    feat(mime): introduce MimeDetector

---
+++ AccountDownloaderLibrary/Mime/CustomTypes.cs
@@ -0,0 +1,28 @@
+﻿using MimeDetective.Storage;
+using System.Collections.Immutable;
+
+namespace AccountDownloaderLibrary.Mime
+{
+    public class CustomTypes
+    {
+        // Based on: https://github.com/MediatedCommunications/Mime-Detective/blob/main/src/MimeDetective/Definitions/Default/FileTypes/Default.FileTypes.Audio.cs
+        public static ImmutableArray<Definition> MESHX()
+        {
+            return new List<Definition>() {
+                new() {
+                    File = new() {
+                        Extensions = new[]{"meshx"}.ToImmutableArray(),
+                        MimeType = "application/meshx",
+                        Categories = new[]{
+                            Category.Lossless,
+                            Category.Audio,
+                        }.ToImmutableHashSet(),
+                    },
+                    Signature = new Segment[] {
+                        PrefixSegment.Create(0, "05 4D 65 73 68 58"),
+                    }.ToSignature(),
+                },
+            }.ToImmutableArray();
+        }
+    }
+}
---
+++ AccountDownloaderLibrary/Mime/MimeDetector.cs
@@ -0,0 +1,49 @@
+﻿using MimeDetective;
+using MimeDetective.Definitions;
+using MimeDetective.Storage;
+using System.Collections.Immutable;
+
+namespace AccountDownloaderLibrary.Mime
+{
+    public class MimeDetector
+    {
+        private static readonly MimeDetector instance = new MimeDetector();
+
+        private ContentInspector inspector = null;
+
+        private MimeDetector()
+        {
+            ImmutableArray<Definition> exhaustiveDefs = new ExhaustiveBuilder()
+            {
+                UsageType = MimeDetective.Definitions.Licensing.UsageType.PersonalNonCommercial
+            }.Build();
+
+            ImmutableArray<Definition>.Builder AllBuildier = ImmutableArray.CreateBuilder<Definition>();
+            AllBuildier.AddRange(exhaustiveDefs);
+            AllBuildier.AddRange(CustomTypes.MESHX());
+
+            var all = AllBuildier.MoveToImmutable();
+
+            inspector = new ContentInspectorBuilder()
+            {
+                Definitions = all
+            }.Build();
+        }
+
+        public string MostLikelyFileExtension(string filePath)
+        {
+            return ChooseMostLikely(inspector.Inspect(filePath).ByFileExtension());
+        }
+
+        public string MostLikelyFileExtension(FileStream stream)
+        {
+            return ChooseMostLikely(inspector.Inspect(stream).ByFileExtension());
+        }
+
+        private string ChooseMostLikely(ImmutableArray<MimeDetective.Engine.FileExtensionMatch> results)
+        {
+            return results.First().Extension;
+        }
+
+    }
+}
