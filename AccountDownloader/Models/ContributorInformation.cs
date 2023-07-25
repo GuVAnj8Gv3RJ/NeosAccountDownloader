47e4e8c42a96ca24e0b7e16684df61664c147dfd
GuVAnj8Gv3RJ <private@private.com>
2023-07-25 01:35:40 -0700

    correct null warnings in contributors

--- AccountDownloader/Models/ContributorInformation.cs
+++ AccountDownloader/Models/ContributorInformation.cs
@@ -6,6 +6,11 @@ public class ContributorsFile
 {
     [JsonPropertyName("contributors")]
     public List<Contributor> Contributors { get; set; }
+
+    public ContributorsFile(List<Contributor> contributors)
+    {
+        Contributors = contributors;
+    }
 }
 
 public class Contributor
@@ -18,4 +23,11 @@ public class Contributor
 
     [JsonPropertyName("avatar_url")]
     public string Avatar { get; set; }
+
+    public Contributor(string name, List<string> contributions, string avatar)
+    {
+        Name = name;
+        Contributions = contributions;
+        Avatar = avatar;
+    }
 }
