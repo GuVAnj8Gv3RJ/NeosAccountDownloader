aa7920989c9e68b8a817351c3536e1994e77bdc0
GuVAnj8Gv3RJ <private@private.com>
2023-09-02 17:57:04 -0700

    fix(featured): encode onlyFeatured only if the user specifies it
    Neos' Cloud will only do filtering on the "featured" state of a world IF the search parameters include it.
    
    Json serialization was including it because CloudX does not implement SearchParameters.OnlyFeatured as a nullable type. Our custom extension of this, resolves this problem.
    
    fixes #192

---
+++ AccountDownloaderLibrary/Models/AccountDownloaderSearchParameters.cs
@@ -0,0 +1,21 @@
+﻿using CloudX.Shared;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace AccountDownloaderLibrary.Models;
+
+/**
+ * CloudX will filter on OnlyFeatured defaulting it to false, even if the user hasn't specified their preference.
+ * 
+ * This is rather silly. So we extend the class and re-define it as a nullable. This allows it to be ignored when sent to the cloud.
+ * 
+ * We'll have to log this as a bug for Neos to fix in CloudX too.
+ */
+
+[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+public class AccountDownloaderSearchParameters : SearchParameters
+{
+    [JsonProperty(PropertyName = "onlyFeatured", NullValueHandling = NullValueHandling.Ignore)]
+    [JsonPropertyName("onlyFeatured")]
+    public new bool? OnlyFeatured { get; set; } = null;
+}
---
+++ AcountDownloaderLibrary.Tests/SearchParameterTests.cs
@@ -0,0 +1,51 @@
+﻿using AccountDownloaderLibrary.Models;
+using Newtonsoft.Json;
+
+namespace AcountDownloaderLibrary.Tests;
+
+
+[TestClass]
+public class SearchParameterTests
+{
+    [TestMethod]
+    public void TestFeaturedNullNewtonSoft()
+    {
+        var ap = new AccountDownloaderSearchParameters();
+
+        ap.OnlyFeatured = null;
+
+        var jsonA = JsonConvert.SerializeObject(ap);
+        var deSerialize = JsonConvert.DeserializeObject<AccountDownloaderSearchParameters>(jsonA);
+
+        Assert.IsNull(deSerialize?.OnlyFeatured, "OnlyFeatured should only be present if the user included it");
+
+        ap.OnlyFeatured = false;
+        jsonA = JsonConvert.SerializeObject(ap);
+
+        deSerialize = JsonConvert.DeserializeObject<AccountDownloaderSearchParameters>(jsonA);
+
+        Assert.IsFalse(deSerialize?.OnlyFeatured, "OnlyFeatured was included and should be false");
+
+    }
+
+    [TestMethod]
+    public void TestFeaturedNullNET()
+    {
+        var ap = new AccountDownloaderSearchParameters();
+
+        ap.OnlyFeatured = null;
+
+        var jsonA = System.Text.Json.JsonSerializer.Serialize(ap);
+        var deSerialize = System.Text.Json.JsonSerializer.Deserialize<AccountDownloaderSearchParameters>(jsonA);
+
+        Assert.IsNull(deSerialize?.OnlyFeatured, "OnlyFeatured should only be present if the user included it");
+
+        ap.OnlyFeatured = false;
+        jsonA = JsonConvert.SerializeObject(ap);
+
+        deSerialize = JsonConvert.DeserializeObject<AccountDownloaderSearchParameters>(jsonA);
+
+        Assert.IsFalse(deSerialize?.OnlyFeatured, "OnlyFeatured was included and should be false");
+
+    }
+}
